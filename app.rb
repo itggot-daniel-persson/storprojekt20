4da7f478004d52983023289ecaf3d3a3b271e78b
itggot-daniel-persson <daniel.persson2@elev.ga.ntig.se>
2020-01-23 15:42:55 +0100

    Added login

--- app.rb
+++ app.rb
@@ -26,19 +26,63 @@ post("/register_new_user") do
     username = params[:username]
     password = params[:password]
     password_confirmation = params[:password_confirmation]
+    phone = params[:phone]
+    email = params[:email]
+
     existing_username = db.execute("SELECT username FROM Users WHERE username = ?", username)
+    existing_email = db.execute("SELECT email FROM Users WHERE email = ?", email)
+    existing_phone = db.execute("SELECT phone FROM Users WHERE phone = ?", phone)
 
     if !existing_username.empty?
         session[:registration_error] = "Username is taken"
+        redirect("/register")
+    elsif password != password_confirmation
+        session[:registration_error] = "Password do not match"
+        redirect("/register")
+    elsif !existing_email.empty?
+        session[:registration_error] = "Email is taken"
+        redirect("/register")
+    elsif !existing_phone.empty?
+        session[:registration_error] = "Phone is taken"
+        redirect("/register")
+    end
+    password_digest = BCrypt::Password.create(password)
+    db.execute("INSERT INTO Users (username, password_digest, rank, email, phone) VALUES (?, ?, ?, ?, ?)", username, password_digest, "user", email, phone)
+    session[:username] = username
+    session[:user_id] = db.execute("SELECT user_id FROM users WHERE username = ?", session[:username])[0]["user_id"]
+    redirect("/")
+end
+
+
+get('/login') do 
+    slim(:"users/index")
+end
+
+post("/login") do
+    username = params[:username]
+    password = params[:password]
+    
+    existing_username = db.execute("SELECT username FROM users WHERE username = ?", username)
+    password_for_user = db.execute("SELECT password_digest FROM users WHERE username = ?", username)[0]["password_digest"]
+    
+    if BCrypt::Password.new(password_for_user) != password || existing_username.empty?
+        session[:login_error] = "Username or password wrong"
         redirect("/")
     end
 
-    if password != password_confirmation
-        session[:registration_error] = "Password do not match"
-        redirect("/")
-    end
-    password_digest = BCrypt::Password.create(password)
-    db.execute("INSERT INTO Users (username, password_digest, rank) VALUES (?, ?, ?)", username, password_digest, 0)
+    session[:user_id] = db.execute("SELECT user_id FROM users WHERE username = ?", username)[0]["user_id"]
     session[:username] = username
-    redirect("/register_success")
+    session[:rank] = db.execute("SELECT rank FROM users WHERE username = ?", username)[0]["rank"]
+    redirect("/")
+end
+
+
+get("/logout") do
+    session[:user_id] = 0
+    session[:chosen_list] = nil
+    session[:rank] = nil
+    session[:username] = nil
+    session[:registration_error] = nil
+    session[:login_error] = nil
+    redirect("/")
 end
